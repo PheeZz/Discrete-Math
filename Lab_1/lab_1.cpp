073c3e48f6bbaabd1f89f5d1daa5442f8a8042b8
PheeZz <jagubkoff@gmail.com>
2022-02-14 12:48:51 +0200

    funcs moved to a separate file

--- Lab_1/lab_1.cpp
+++ Lab_1/lab_1.cpp
@@ -1,31 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <bits/stdc++.h>
-
-using namespace std;
-
-void input(string in, vector<char> &vec) //функция для заполнения векторов (множеств)
-{
-    cout << "input new arr: \n";
-    cin >> in;                  //вводим вспомогательную строку
-    sort(in.begin(), in.end()); //сортируем ее
-    for (int i = 0; i < in.size(); i++)
-    {
-        vec.push_back(in[i]); //заполняет вектор char'ов посимвольно из отсортированной строки
-    }
-    in.clear();
-    cout << '\n';
-}
-
-void printVec(vector<char> vec) //функция для вывода содержимого множества
-{
-    for (int i = 0; i < vec.size(); i++)
-    {
-        cout << vec[i];
-    }
-    cout << '\n';
-}
+#include "functions.h"
 
 int main()
 {
